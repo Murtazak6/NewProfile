f68728087acada74b0ac4ceabc445cef3065737c
Murtaza K <m.kankoliwala@somaiya.edu>
2022-10-05 15:57:08 +0530

    new changes

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -1,7 +1,73 @@
 import React, { Component } from "react";
 import { Fade, Slide } from "react-reveal";
-
+import emailjs from 'emailjs-com'
+// import { toast } from 'react-toastify';
 class Contact extends Component {
+
+  constructor(props){
+    super(props)
+    this.state = {
+      name: '',
+      email: '',
+      subject: '',
+      content:''
+    }
+    this.handleChange = this.handleChange.bind(this)
+    this.handleChangeName = this.handleChangeName.bind(this)
+    this.handleChangeEmail = this.handleChangeEmail.bind(this)
+    this.handleChangeSubject = this.handleChangeSubject.bind(this)
+  }
+
+  sendEmail = (e) => {
+      if((this.state.content !== '' && this.state.content !== null && this.state.content !== undefined) && 
+          (this.state.name !== '' && this.state.name !== null && this.state.name !== undefined) && 
+          (this.state.email !== '' && this.state.email !== null && this.state.email !== undefined) && 
+          (this.state.subject !== '' && this.state.subject !== null && this.state.subject !== undefined)){
+          e.preventDefault();
+          emailjs.send('service_sc6hxk5', 'template_qehbtoy', {
+              name: this.state.name,
+              email: this.state.email,
+              subject: this.state.subject,
+              message: this.state.content
+          }, 'user_minqCOuSYQdQPwDjoVYj9')
+          .then((result) => {
+              console.log(result.text);
+              this.setState({
+                name:'',
+                content:'',
+                email:'',
+                subject:''
+              })
+              alert('Your message is sent. Please wait...\n Murtaza will contact you soon.')
+          }, (error) => {
+              console.log(error.text);
+          });
+      } else {
+          alert('Please enter all fields')
+      }
+  }
+  
+  handleChangeName = (e) => {
+    this.setState({
+      name: e.target.value
+    })
+  }
+  handleChangeEmail = (e) => {
+    this.setState({
+      email: e.target.value
+    })
+  }
+  handleChangeSubject = (e) => {
+    this.setState({
+      subject: e.target.value
+    })
+  }
+  handleChange = (e) => {
+    this.setState({
+      content: e.target.value
+    })
+  }
+
   render() {
     if (!this.props.data) return null;
 
@@ -15,7 +81,7 @@ class Contact extends Component {
 
     return (
       <section id="contact">
-        <Fade bottom duration={1000}>
+        <Slide right duration={1}>
           <div className="row section-head">
             <div className="two columns header-col">
               <h1>
@@ -27,7 +93,7 @@ class Contact extends Component {
               <p className="lead">{message}</p>
             </div>
           </div>
-        </Fade>
+        </Slide>
 
         <div className="row">
           <Slide left duration={1000}>
@@ -35,59 +101,61 @@ class Contact extends Component {
               <form action="" method="post" id="contactForm" name="contactForm">
                 <fieldset>
                   <div>
-                    <label htmlFor="contactName">
+                    <label htmlFor="name">
                       Name <span className="required">*</span>
                     </label>
                     <input
                       type="text"
-                      defaultValue=""
+                      value={this.state.name}
                       size="35"
-                      id="contactName"
-                      name="contactName"
-                      onChange={this.handleChange}
+                      id="name"
+                      name="name"
+                      onChange={this.handleChangeName}
                     />
                   </div>
 
                   <div>
-                    <label htmlFor="contactEmail">
+                    <label htmlFor="email">
                       Email <span className="required">*</span>
                     </label>
                     <input
                       type="text"
-                      defaultValue=""
+                      value={this.state.email}
                       size="35"
-                      id="contactEmail"
-                      name="contactEmail"
-                      onChange={this.handleChange}
+                      id="email"
+                      name="email"
+                      onChange={this.handleChangeEmail}
                     />
                   </div>
 
                   <div>
-                    <label htmlFor="contactSubject">Subject</label>
+                    <label htmlFor="subject">Subject</label>
                     <input
                       type="text"
-                      defaultValue=""
+                      value={this.state.subject}
                       size="35"
-                      id="contactSubject"
-                      name="contactSubject"
-                      onChange={this.handleChange}
+                      id="subject"
+                      name="subject"
+                      onChange={this.handleChangeSubject}
                     />
                   </div>
 
                   <div>
-                    <label htmlFor="contactMessage">
+                    <label htmlFor="message">
                       Message <span className="required">*</span>
                     </label>
                     <textarea
                       cols="50"
                       rows="15"
-                      id="contactMessage"
-                      name="contactMessage"
+                      value={this.state.content}
+                      id="message"
+                      name="content"
+                      onChange={this.handleChange}
                     ></textarea>
                   </div>
 
                   <div>
-                    <button className="submit">Submit</button>
+                    <button className="submit" onClick={this.sendEmail}>Submit</button>
                     <span id="image-loader">
                       <img alt="" src="images/loader.gif" />
                     </span>
@@ -103,7 +171,7 @@ class Contact extends Component {
             </div>
           </Slide>
 
-          <Slide right duration={1000}>
+          {/* <Slide right duration={1000}>
             <aside className="four columns footer-widgets">
               <div className="widget widget_contact">
                 <h4>Address and Phone</h4>
@@ -146,7 +214,7 @@ class Contact extends Component {
                 </ul>
               </div>
             </aside>
-          </Slide>
+          </Slide> */}
         </div>
       </section>
     );
--- src/Components/Header.js
+++ src/Components/Header.js
@@ -10,10 +10,21 @@ class Header extends Component {
     const github = this.props.data.github;
     const name = this.props.data.name;
     const description = this.props.data.description;
-
+    // "color"
+    // "ball"
+    // "lines"
+    // "thick"
+    // "circle"
+    // "cobweb"
+    // "polygon"
+    // "square"
+    // "tadpole"
+    // "fountain"
+    // "random"
+    // "custom"
     return (
       <header id="home">
-        <ParticlesBg type="circle" bg={true} />
+        <ParticlesBg type="cobweb" color="#393186" num={150} bg={true} />
 
         <nav id="nav-wrap">
           <a className="mobile-btn" href="#nav-wrap" title="Show navigation">
@@ -42,11 +53,11 @@ class Header extends Component {
               </a>
             </li>
 
-            <li>
+            {/* <li>
               <a className="smoothscroll" href="#portfolio">
-                Works
+                Projects
               </a>
-            </li>
+            </li> */}
 
             <li>
               <a className="smoothscroll" href="#contact">
@@ -62,17 +73,17 @@ class Header extends Component {
               <h1 className="responsive-headline">{name}</h1>
             </Fade>
             <Fade bottom duration={1200}>
-              <h3>{description}.</h3>
+              <h3>{description}</h3>
             </Fade>
             <hr />
             <Fade bottom duration={2000}>
               <ul className="social">
-                <a href={project} className="button btn project-btn">
+                {project ? <a href={project} className="button btn project-btn">
                   <i className="fa fa-book"></i>Project
-                </a>
-                <a href={github} className="button btn github-btn">
+                </a>: null}
+                {github ? <a href={github} className="button btn github-btn">
                   <i className="fa fa-github"></i>Github
-                </a>
+                </a>:null}
               </ul>
             </Fade>
           </div>
